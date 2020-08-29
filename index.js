6b5289d27d28198426975f2b19b111475f768ef3
Max Horstmann <max.horstmann@gmail.com>
2020-08-29 14:00:58 -0400

    works

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var components = [
     image: "mcr.microsoft.com/dotnet/core/sdk",
     tags: ["latest"],
     //ports: ["8000:80"],
-    volumes: "./src:/usr/src",
+    volumes: [ "./src:/usr/src" ],
     stdin_open: true,
     tty: true,
     working_dir: "/usr/src"
@@ -43,11 +43,43 @@ var app = new Vue({
 
     methods: {
         update: function() {
-            lines = ["version: \"3\"", "services:", ""]
+            lines = ["version: \"3\"", "services:"]
             for (var i=0; i<components.length; i++) {
                 var component = components[i];
                 if (!component.checked) continue;
                 lines.push("\t" + component.name + ":")
+                lines.push("\t\tcontainer_name: " + component.name)
+                // TODO select version tag
+                lines.push("\t\timage: " + component.image + ":" + component.tags[component.tags.length-1])
+                lines.push("\t\tcontainer_name: " + component.name)
+                if (component.ports) {
+                    lines.push("\t\tports: ")
+                    for (var j=0; j<component.ports.length;j++) {
+                        lines.push("\t\t- " + component.ports[j])
+                    }
+                }
+                if (component.environment) {
+                    lines.push("\t\tenvironment: ")
+                    for (var j=0; j<component.environment.length;j++) {
+                        lines.push("\t\t- " + component.environment[j])
+                    }
+                }
+                if (component.volumes) {
+                    lines.push("\t\tvolumes: ")
+                    for (var j=0; j<component.volumes.length;j++) {
+                        lines.push("\t\t- " + component.volumes[j])
+                    }
+                }
+                if (component.stdin_open) {
+                    lines.push("\t\tstdin_open: " + component.stdin_open)
+                }
+                if (component.tty) {
+                    lines.push("\t\ttty: " + component.tty)
+                }
+                if (component.working_dir) {
+                    lines.push("\t\tworking_dir: " + component.working_dir)
+                }
+
             }
             this.dockercompose = lines.join("\n")
         },
